36a9c98220e7de026fd5148a0ab6177bba89a14c
SubirChDey <subirdey93@gmail.com>
2025-01-25 22:08:29 +0600

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default MainLayout
